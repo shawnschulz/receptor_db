dde441dead477eb937ddfc096389c080c5f43522
shawnschulz <shawnschulz@gberkeley.edu>
2025-06-09 22:01:25 -0700

    Add compose.yaml

--- rs_receptor_db/src/main.rs
+++ rs_receptor_db/src/main.rs
@@ -4,9 +4,15 @@ use axum::{
         Json,
 };
 use std::net::SocketAddr;
-use curl::easy::Easy;
+use reqwest;
 use std::io::{stdout, Write};
 use log::{info, warn, error};
+use std::string;
+use serde_json::{Result, Value};
+
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
 
 #[tokio::main]
 async fn main() {
@@ -15,14 +21,16 @@ async fn main() {
         .route("/get_var", get(get_var));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     info!("Server running at http://{}", addr);
-    stdout().write_all("blah blah balh \n".as_bytes()).unwrap();
     stdout().flush().unwrap();
+    // Spin up connection to postgres db running over docker
+
     _convert_gene_json_db().await;
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
+
 
 // Calls the uniprot API using curl and then converts the resulting
 // json into a format that either is a DB or can easily be written
@@ -31,28 +39,23 @@ async fn main() {
 async fn _convert_gene_json_db() {
     // Call GO API using cURL
     // handling a failed call to the api???
-    let mut easy = Easy::new();
+    // Store data in a json value
     let url = "https://api.geneontology.org/api/bioentity/function/GO:0007166?category=gene&taxon_label=Homo%20sapiens&rows=1000000";
-    easy.url(url).unwrap();
-    // Use curl client to get url text json response
-    easy.get(false).unwrap();
-    easy.write_function(|data| {
-        Ok(stdout().write(data).unwrap_or_else(|error| {
-            error!("Problem writing the file: {:?}", error);
-            1
-        }))
-    }).unwrap();
-    easy.perform().unwrap();
-    stdout().flush().unwrap();
-//    println!("{:?}", data);
+    let res = reqwest::get(url).await.unwrap().text().await.unwrap();
+    // Use curl client to get url utf-8 encoded byte stream json response
+    let json_value: Vec<Value> = serde_json::from_str(&res).unwrap();
+    // Add data from stack local buffer as records to a postgres db
+    for item in &json_value {
+    };
 
-    // Get a json string from the api result
-
-    // Reformat in a way that makes sense
-    
-    // Add data as records to a postgres db
-    
     // Return a Result
+}
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("Must set the database  url");
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
 
 async fn homepage() -> &'static str {
